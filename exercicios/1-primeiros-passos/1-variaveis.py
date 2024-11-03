1de0f4dca5137a7f87869058fd6ab5e2e490ac28
Edgar Aracnidio <edgarmapanzene@gmail.com>
2024-11-03 12:29:46 +0000

    Exercicios Variaveis

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,12 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
-
+nome = "Edgar"
+cidade = "Maputo"
 # Declare variáveis de cada tipo numérico
-
+idade = 25           # Inteiro (int)
+altura = 1.75        # Ponto flutuante (float)
+peso = 70.5          # Ponto flutuante (float)
 
 # Declare uma variável do tipo booleano
+estudando_python = True
